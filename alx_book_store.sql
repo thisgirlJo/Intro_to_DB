c0a9b0725a79e0ca68e420f22a11d44559fbddb6
Judith Ogbole <judithogbole71@gmail.com>
2024-10-18 15:33:21 +0100

    An intro to DB- Task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,8 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
     CREATE TABLE Books(
         book_id INT PRIMARY KEY NOT NULL,
         title VARCHAR(130) NOT NULL,
-        FOREIGN KEY (author_id)
-    REFERENCES Authors(author_id),
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id),
         price DOUBLE NOT NULL,
         publication_date DATE NOT NULL,
     );
@@ -22,16 +21,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
     CREATE TABLE Orders(
         order_id INT PRIMARY KEY NOT NULL,
-        FOREIGN KEY (customer_id)
-    REFERENCES Customers(customer_id),
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
         order_date DATE
     );
 
     CREATE TABLE Order_Details(
         orderdetailid INT PRIMARY KEY NOT NULL,
-        FOREIGN KEY (order_id)
-    REFERENCES Orders(order_id),
-        FOREIGN KEY (book_id)
-    REFERENCES bOOKS(book_id),
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES bOOKS(book_id),
         quantity DOUBLE.
     );
