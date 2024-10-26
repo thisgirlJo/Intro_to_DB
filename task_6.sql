90924c47263f595ac5832f0eda241c23c9fa50e2
Judith Ogbole <judithogbole71@gmail.com>
2024-10-26 11:53:34 +0100

    Task 6\

--- task_6.sql
+++ task_6.sql
@@ -8,13 +8,13 @@ mydb = mysql.connector.connect(
     )
 mycursor = mydb.cursor()
 
-add = "INSERT INTO Customers(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
+sql = "INSERT INTO Customers(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
 
 val = [
-    ('1', "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave")
-    ('2', "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave")
-    ('3', "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave")
-    ('4', "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave")
+    ('1', "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+    ('2', "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.")
+    ('3', "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave.")
+    ('4', "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
 ]
-mycursor.execute(add, val)
+mycursor.execute(sql, val)
 mydb.commit()
