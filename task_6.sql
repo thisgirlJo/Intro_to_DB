1a1fc297cb226fb3b8ec3dc4cf92cdc68400446c
Judith Ogbole <judithogbole71@gmail.com>
2024-10-26 11:55:21 +0100

    Task 6\

--- task_6.sql
+++ task_6.sql
@@ -8,7 +8,7 @@ mydb = mysql.connector.connect(
     )
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO Customers(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
+sql = "INSERT INTO customers(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
 
 val = [
     ('1', "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
